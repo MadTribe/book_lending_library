cf428c2791d1e5a13f52f4b1cdf43ca5a0bf67ad
MadTribe <MadTribe@users.noreply.github.com>
2026-02-02 20:50:48 +0000

    Test out 2nd order search functions

--- src/main/kotlin/com/patchwork/booklibrary/repositories/InMemoryBooksRepository.kt
+++ src/main/kotlin/com/patchwork/booklibrary/repositories/InMemoryBooksRepository.kt
@@ -1,20 +1,40 @@
 package com.patchwork.booklibrary.repositories
 
 import com.patchwork.booklibrary.model.Book
+import kotlin.reflect.KClass
 
-class InMemoryBooksRepository(var books : List<Book>) : BooksRepository  {
-    override fun findBooksByAuthor(author: String): List<Book> {
-        return books.filter {
-            it.author
-                .indexOf(author, 0, true) >= 0
+
+fun caseInsensitiveMatchAnywhere(fieldName: String, data: List<Book>): (searchTerm: String) -> List<Book> {
+
+    val getProp = fun(b: Book): String {
+
+        val t = b.javaClass
+            .declaredFields
+            .find { it.name == fieldName }
+            ?.also { it.trySetAccessible() }
+            ?.get(b)
+
+        return t.toString()
+    }
+
+    return fun(searchTerm: String): List<Book> {
+        return data.filter {
+            getProp(it)
+                .indexOf(searchTerm, 0, true) >= 0
         }
+    }
+}
+
+class InMemoryBooksRepository(var books: List<Book>) : BooksRepository {
+    val findBooksByAuthorImpl: (author: String) -> List<Book> = caseInsensitiveMatchAnywhere("author", books)
+    val findBooksByTitleImpl: (author: String) -> List<Book> = caseInsensitiveMatchAnywhere("title", books)
+
+    override fun findBooksByAuthor(author: String): List<Book> {
+        return findBooksByAuthorImpl(author)
     }
 
     override fun findBooksByTitle(title: String): List<Book> {
-        return books.filter {
-            it.title
-                .indexOf(title, 0, true) >= 0
-        }
+        return findBooksByTitleImpl(title)
     }
 
     override fun findBooksByISBN(isbn: String): List<Book> {
@@ -32,11 +52,11 @@ class InMemoryBooksRepository(var books : List<Book>) : BooksRepository  {
     override fun updateBook(new: Book) {
         books = books.map {
             if (it.libraryItemId == new.libraryItemId) new else it
-         }
+        }
     }
 
     override fun findBooksLoanedTo(userId: String): List<Book> {
-       return books.filter { it.borrower?.id == userId }
+        return books.filter { it.borrower?.id == userId }
     }
 
     override fun findBorrowedBooks(): List<Book> {
